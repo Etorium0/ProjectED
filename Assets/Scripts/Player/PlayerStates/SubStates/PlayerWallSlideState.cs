1ea25736e0541c627351aa11d9a81fbe804c2456
unknown <22520882@gm.uit.edu.vn>
2025-05-13 00:18:16 +0700

    update state

---
+++ Assets/Scripts/Player/PlayerStates/SubStates/PlayerWallSlideState.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerWallSlideState : PlayerTouchingWallState
+{
+    public PlayerWallSlideState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        player.SetVelocityY(-playerData.wallSlideVelocity);
+
+        if (grabInput && yInput == 0)
+        {
+            stateMachine.ChangeState(player.WallGrabState);
+        }
+
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+}
