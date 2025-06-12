9284374e62125e5ae07e5763464bc7f66a501f67
unknown <22520882@gm.uit.edu.vn>
2025-06-12 16:18:34 +0700

    add something weird for future weapon generate

--- Assets/_Scripts/ScriptableObjects/WeaponDataSO.cs
+++ Assets/_Scripts/ScriptableObjects/WeaponDataSO.cs
@@ -8,19 +8,21 @@ namespace Etorium.Weapons
     [CreateAssetMenu(fileName = "newWeaponData", menuName = "Data/Weapon Data/Basic Weapon Data", order = 0)]
     public class WeaponDataSO : ScriptableObject
     {
-        [field: SerializeField] public int NumberOfAttacks {get; private set;}
-        
-        [field: SerializeReference] public List<ComponentData> ComponentData {get; private set;}
+        [field: SerializeField] public int NumberOfAttacks { get; private set; }
+
+        [field: SerializeReference] public List<ComponentData> ComponentData { get; private set; }
 
         public T GetData<T>()
         {
             return ComponentData.OfType<T>().FirstOrDefault();
         }
-        
-        [ContextMenu("Add Sprite Data")]
-        private void AddSpriteData() => ComponentData.Add(new WeaponSpriteData());
-        
-        [ContextMenu("Add Movement Data")]
-        private void AddMovementData() => ComponentData.Add(new MovementData());
+
+        public void AddData(ComponentData data)
+        {
+            if(ComponentData.FirstOrDefault(t => t.GetType() == data.GetType()) != null) 
+                return;
+            
+            ComponentData.Add(data);
+        }
     }
 }
