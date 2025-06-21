2ec1142e1308de8899bf835d43bca8a857f117fe
unknown <22520882@gm.uit.edu.vn>
2025-06-21 16:49:41 +0700

    add concept of projectilespawnerstrategy

---
+++ Assets/_Scripts/Weapons/ProjectileSpawnerStrategy/IProjectileSpawnerStrategy.cs
@@ -0,0 +1,16 @@
+﻿﻿using System;
+using Etorium.ObjectPoolSystem;
+using Etorium.ProjectileSystem;
+using Etorium.Weapons.Components;
+using UnityEngine;
+
+namespace Etorium.Weapons
+{
+    public interface IProjectileSpawnerStrategy
+    {
+        void SpawnProjectile(ProjectileSpawnInfo projectileSpawnInfo, Vector3 spawnerPos, int facingDirection,
+            ObjectPools objectPools, Action<Projectile> OnSpawnProjectile);
+        
+        
+    }
+}
